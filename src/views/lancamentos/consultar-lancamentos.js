e3572218d3a09c84425d6f92a9ef8765b4c7a199
Romaomoura <c.romaomoura@gmail.com>
2021-12-28 20:39:30 -0300

    Implementando confirmação antes de deletar lançamento.

--- src/views/lancamentos/consultar-lancamentos.js
+++ src/views/lancamentos/consultar-lancamentos.js
@@ -9,6 +9,9 @@ import LancamentosTable from './lancamentosTable'
 import LancamentoService from '../../app/service/lancamentoService'
 import LocalStorageService from '../../app/service/localStoreService'
 
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+
 import * as messages from '../../components/toastr'
 
 class ConsultarLancamentos extends React.Component {
@@ -18,6 +21,8 @@ class ConsultarLancamentos extends React.Component {
         mes : '',
         tipo : '',
         descricao: '',
+        showConfirmDialog: false,
+        lancamentoADeletar: {},
         lancamentos: []
     }
 
@@ -55,13 +60,23 @@ class ConsultarLancamentos extends React.Component {
         console.log('Editando o lancamento ', id)
     }
 
-    deletar = (lancamento) => {
-        this.service.deletar(lancamento.id).then(response => {
-            const lancamentos = this.state.lancamentos;
-            const index = lancamentos.indexOf(lancamento);
-            lancamentos.splice(index, 1);
-            this.setState(lancamentos);
-            messages.mensagemSucesso('Lançamento deletado com sucesso.')
+    abrirConfirmacao = (lancamento) => {
+        this.setState({showConfirmDialog: true, lancamentoADeletar: lancamento})
+    }
+
+    cancelarConfirmacao = (lancamento) => {
+        this.setState({showConfirmDialog: false, lancamentoADeletar: {}})
+    }
+
+    deletar = () => {
+        this.service
+        .deletar(this.state.lancamentoADeletar.id)
+        .then(response => {
+                const lancamentos = this.state.lancamentos;
+                const index = lancamentos.indexOf(this.state.lancamentoADeletar);
+                lancamentos.splice(index, 1);
+                this.setState({lancamentos: lancamentos, showConfirmDialog: false});
+                messages.mensagemSucesso('Lançamento deletado com sucesso.')
         }).catch(error => {
             messages.mensagemErro('Ocorreu um erro ao tentar deletar o lancamento.')
         })
@@ -72,6 +87,13 @@ class ConsultarLancamentos extends React.Component {
     const lista = this.service.obterListaMeses();
 
     const tipos = this.service.obterListaTipos();
+
+    const confirmDialogFooter = (
+        <div>
+            <Button label="Confirmar" icon="pi pi-check" onClick={this.deletar} />
+            <Button label="Cancelar" icon="pi pi-times" onClick={this.cancelarConfirmacao} className="p-button-secondary"/>
+        </div>
+    );
 
 
        return (
@@ -112,10 +134,21 @@ class ConsultarLancamentos extends React.Component {
                    <div className="col-md-12">
                        <div className="bs-component">
                             <LancamentosTable lancamentos={this.state.lancamentos}
-                                              deleteAction={this.deletar}
+                                              deleteAction={this.abrirConfirmacao}
                                               editAction={this.editar} />
                        </div>
                    </div>
+               </div>
+               <div>
+               <Dialog header="Atenção" 
+                        visible={this.state.showConfirmDialog} 
+                        style={{ width: '50vw' }} 
+                        modal={true}
+                        footer={confirmDialogFooter}
+                        onHide={() => this.setState({showConfirmDialog: false})}>
+                    <p>Deseja realmente deletar o lancamento?</p>
+                </Dialog>
+
                </div>
            </Card>
        )
