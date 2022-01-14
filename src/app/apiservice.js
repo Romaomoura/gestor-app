68784b05a237b0f2ded5f98f95dfeb2a827a024e
Romaomoura <c.romaomoura@gmail.com>
2022-01-14 19:50:03 -0300

    Ajustes na conexão de rota com api

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const httpCliente = axios.create(
     {
-        baseURL: 'https://gfinc-api.herokuapp.com'
+        baseURL: 'https://gfinc-api.herokuapp.com/'
     }
 )
 class ApiService {
