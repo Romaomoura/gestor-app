afbaa90ffb199f1219ac8a7e67832f54c8428360
Romaomoura <c.romaomoura@gmail.com>
2022-03-15 20:01:37 -0300

    Alterando rota para produçao

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; 
 
-const baseURL = process.env.REACT_APP_API_URL
+const baseURL = process.env.production.REACT_APP_API_URL /* process.env.REACT_APP_API_URL */
 
 export const httpClient = axios.create({
     baseURL: baseURL,
